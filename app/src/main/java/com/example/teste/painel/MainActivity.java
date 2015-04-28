e1bcc9bef87395340837e314de09460bc391a975
tsilvadeoliveira <tsilvadeoliveira@gmail.com>
2015-04-28 16:44:54 -0300

    Initial commit

---
+++ app/src/main/java/com/example/teste/painel/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.teste.painel;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+private Button btnChamdos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnChamdos = (Button) findViewById(R.id.btnChamados);
+        btnChamdos.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, PainelActivity.class);
+                startActivityForResult(i, 1);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
